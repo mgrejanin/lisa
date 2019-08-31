cc08e99e9e9756956ce08318ee376970fbd9d484
Mauricio Grejanin <mgrejanin@eurekaria.com>
2019-08-31 00:47:21 -0300

    mercados proximos ok

--- libs/shared/ui/thumbnail-carroussel/src/lib/shared-ui-thumbnail-carroussel.component.ts
+++ libs/shared/ui/thumbnail-carroussel/src/lib/shared-ui-thumbnail-carroussel.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { ThumbnailCarrousselType } from '../symbols';
 
 @Component({
   selector: 'lisa-shared-ui-thumbnail-carrousel',
   templateUrl: './shared-ui-thumbnail-carroussel.component.html',
-  styleUrls: ['./shared-ui-thumbnail-carroussel.component.scss']
+  styleUrls: ['./shared-ui-thumbnail-carroussel.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SharedUiThumbnailCarrousselComponent {
   @Input() roundedThumbnail: boolean;
--- libs/shared/ui/thumbnail-carroussel/src/lib/shared-ui-thumbnail-carroussel.module.ts
+++ libs/shared/ui/thumbnail-carroussel/src/lib/shared-ui-thumbnail-carroussel.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedUiThumbnailCarrousselComponent } from './shared-ui-thumbnail-carroussel.component';
+import { MatListModule } from '@angular/material';
 
 @NgModule({
   declarations: [SharedUiThumbnailCarrousselComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatListModule],
   exports: [SharedUiThumbnailCarrousselComponent]
 })
 export class SharedUiThumbnailCarrousselModule {}
